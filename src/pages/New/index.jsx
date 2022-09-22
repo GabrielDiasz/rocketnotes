f824af029ae88ccbc025367ed5cf27b002c01393
Gabriel Dias <gabrielpereiradias5@gmail.com>
2022-09-22 10:57:51 -0300

    import textarea in page New

--- src/pages/New/index.jsx
+++ src/pages/New/index.jsx
@@ -2,6 +2,7 @@ import { Container, Form } from "./styles"
 
 import { Header } from "../../components/Header"
 import { Input } from "../../components/Input"
+import { Textarea } from "../../components/Textarea"
 
 export function New() {
   return (
@@ -18,6 +19,8 @@ export function New() {
           <Input
             placeholder="Título"
           />
+
+          <Textarea placeholder="Observações"/>
 
         </Form>
       </main>
