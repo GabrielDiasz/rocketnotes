38b39b8702d1d04621b6d02602196fe9629fae0d
Gabriel Dias <gabrielpereiradias5@gmail.com>
2022-10-07 10:46:24 -0300

    added api

--- src/routes/app.routes.jsx
+++ src/routes/app.routes.jsx
@@ -4,14 +4,17 @@ import { New } from "../pages/New"
 import { Home } from "../pages/Home"
 import { Profile } from "../pages/Profile"
 import { Details } from "../pages/Details"
+import { SignUp } from "../pages/SignUp"
+import { SignIn } from "../pages/SignIn"
 
 export function AppRoutes() {
   return (
     <Routes>
-      <Route path="/" element={<Home/>} />
+      <Route path="/" element={<SignIn />} />
       <Route path="/new" element={<New/>} />
       <Route path="/profile" element={<Profile/>} />
       <Route path="/details/:id" element={<Details/>} />
+      <Route path="/register" element={<SignUp />} />
     </Routes>
   )
 }
